7d4d500b36571db6b1a696ec2ec5e19ecbbd8e0c
MaishaFN <jose.maisha@gmail.com>
2023-05-12 16:16:07 +0200

    Admin routes finished

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -5,15 +5,41 @@ const Class = require ('../models/Class.model')
 const {isAdmin} = require('../middlewares/middlewares');
 
 //GET /admin
-
 router.get("/", async(req, res, next)=>{
 try {
-    
-} catch (error) {
-    
-}
-    
-    
+    const allUsers = await User.find()
+    res.render("admin/users.hbs",{allUsers:allUsers})
+} catch (error) {next(error)}
 })
+
+//GET /admin/:idUser
+router.get("/:idUser", async (req, res, next)=>{
+try {
+    const oneUser = await User.findById(req.params.idUser)
+    res.render("admin/users-edit.hbs",{oneUser:oneUser})
+} catch (error) {
+    next(error)
+}
+})
+
+//POST /admin/:idUser
+router.post("/:idUser", async(req, res, next)=>{
+    try {
+        await User.findByIdAndUpdate(req.params.idUser)
+        res.render("admin/users-edit.hbs")
+    } catch (error) {
+        next(error)
+    }
+    })
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
