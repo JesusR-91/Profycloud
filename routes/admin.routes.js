7bd353f0d96fd923c2a1e5f382145418f7a6db4c
MaishaFN <jose.maisha@gmail.com>
2023-05-12 15:28:06 +0200

    Classes done

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
+const User = require('../models/User.model');
+const Class = require ('../models/Class.model')
 
 const {isAdmin} = require('../middlewares/middlewares');
 
+//GET /admin
 
-
-
-
-
+router.get("/", async(req, res, next)=>{
+try {
+    
+} catch (error) {
+    
+}
+    
+    
+})
 
 module.exports = router;
--- routes/classes.routes.js
+++ routes/classes.routes.js
@@ -1,14 +1,37 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const Class = require("../models/Class.model.js");
 
+//GET /class
+router.get("/", async (req, res, next) => {
+  try {
+    const allClasses = await Class.find();
+    res.render("classes/index.hbs", {
+      allClasses: allClasses,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-//GET /clases
+//GET /class/:idClase
+router.get("/:idClass", async (req, res, next) => {
+  try {
+    const oneClass = await Class.findById(req.params.idClase);
+    res.render("classes/class.hbs", { oneClass: oneClass });
+  } catch (error) {
+    next(error);
+  }
+});
 
-
-
-//POST /clases/:idClase
-
-
-
+//POST /class/:idClase
+router.post("/:idClass", async(req, res, next)=>{
+    try {
+        await Class.findByIdAndUpdate(req.params.idClase)
+        res.redirect("/class")
+    } catch (error) {
+        next(error)
+    }
+})
 
 
 
