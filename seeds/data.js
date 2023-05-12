1585fef0d9fad3d811988ec980703f1fb5063f0e
Jesús Ruiz <jesusruizjaen@gmail.com>
2023-05-12 15:30:57 +0200

    some data included

---
+++ seeds/data.js
@@ -0,0 +1,98 @@
+const classes = [
+    {name: 1,subName: "A", Subject: ["Matemáticas", "Física", "Programación", "Tecnología"]},
+    {name: 1,subName: "B", Subject: ["Matemáticas", "Física", "Programación", "Tecnología"]},
+    {name: 2,subName: "A", Subject: ["Biología", "Química", "Educación física", "Tecnología"]},
+    {name: 2,subName: "B", Subject: ["Biología", "Química", "Educación física", "Tecnología"]},
+    {name: 3,subName: "A", Subject: ["Química", "física", "programación", "Biología"]},
+    {name: 3,subName: "B", Subject: ["Química", "física", "programación", "Biología"]},
+    {name: 4,subName: "A", Subject: ["Biología", "Física", "Química", "Tecnología"]},
+    {name: 4,subName: "B", Subject: ["Biología", "Física", "Química", "Tecnología"]},
+]
+
+const alumns =[
+    {firstName: "Juan", lastName: "Bolsón", class: []},
+    {firstName: "Pedro", lastName: "Skywalker", class: []},
+    {firstName: "Jorge", lastName: "Vader", class: []},
+    {firstName: "Jesús", lastName: "González", class: []},
+    {firstName: "Jonathan", lastName: "Benítez", class: []},
+    {firstName: "Maisha", lastName: "Ramírez", class: []},
+    {firstName: "Jorge", lastName: "Ruiz", class: []},
+    {firstName: "Carol", lastName: "Pérez", class: []},
+    {firstName: "Jorge", lastName: "González", class: []},
+    {firstName: "Ana", lastName: "Pérez", class: []},
+    {firstName: "Rosa", lastName: "Navarro", class: []},
+    {firstName: "Andrea", lastName: "Ramírez", class: []},
+    {firstName: "Frodo", lastName: "Benítez", class: []},
+    {firstName: "Ana", lastName: "Ruiz", class: []},
+    {firstName: "Andrea", lastName: "Navarro", class: []},
+    {firstName: "Frodo", lastName: "Bolsón", class: []},
+    {firstName: "Jonathan", lastName: "Ruiz", class: []},
+    {firstName: "Pedro", lastName: "Bolsón", class: []},
+    {firstName: "Frodo", lastName: "Vader", class: []},
+    {firstName: "Carol", lastName: "Ruiz", class: []},
+    {firstName: "Rosa", lastName: "Pérez", class: []},
+    {firstName: "Frodo", lastName: "Navarro", class: []},
+    {firstName: "Ana", lastName: "Benítez", class: []},
+    {firstName: "Pedro", lastName: "Ramírez", class: []},
+    {firstName: "Jesús", lastName: "Navarro", class: []},
+    {firstName: "Rosa", lastName: "Skywalker", class: []},
+]
+
+const professors = [
+    {
+    email: "jesus@jesus.com", 
+    password: "Patata99", 
+    firstName: "Jesús", 
+    lastName: "Ruiz", 
+    rol:['professor', 'tutor'],
+    admin: false,
+    class: [],
+  },
+  {
+    email: "maisha@maisha.com", 
+    password: "Patata99", 
+    firstName: "Maisha", 
+    lastName: "Fumanal",  
+    rol:['professor'],
+    admin: true,
+    class: [],
+  },
+  {
+    email: "jorge@jorge.com", 
+    password: "Patata99", 
+    firstName: "Jorge", 
+    lastName: "Berrizbeitia", 
+    rol:['professor'],
+    admin: true,
+    class: [],
+  }
+]
+
+const mongoose = require('mongoose');
+const User = require('../models/User.model');
+const Class = require('../models/Class.model');
+const Alumn = require('../models/Alumn.model');
+
+require('../db/index');
+
+User.insertMany(professors)
+.then(()=>{
+console.log('Usuarios metidos en base de dato')
+})
+.catch(err => console.log(err))
+
+Class.insertMany(classes)
+.then(()=>{
+console.log('Clases metidos en base de dato')
+})
+.catch(err => console.log(err))
+
+Alumn.insertMany(alumns)
+.then(()=>{
+console.log('Alumnos metidos en base de dato')
+mongoose.connection.close();
+})
+.catch(err => console.log(err))
+
+
+
