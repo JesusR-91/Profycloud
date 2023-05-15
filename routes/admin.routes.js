34d944adaf70f33e0f1595dd252db4753c78e8b0
MaishaFN <jose.maisha@gmail.com>
2023-05-15 15:05:47 +0200

    admin finished

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
 const Class = require("../models/Class.model");
+const uploader = require('../middlewares/cloudinary.middleware.js');
 
 const { isAdmin } = require("../middlewares/middlewares");
 
@@ -8,7 +9,7 @@ const { isAdmin } = require("../middlewares/middlewares");
 router.get("/", async (req, res, next) => {
   try {
     const allUsers = await User.find();
-    res.render("admin/index", { allUsers: allUsers });
+    res.render("admin/index", {allUsers});
   } catch (error) {
     next(error);
   }
@@ -18,7 +19,7 @@ router.get("/", async (req, res, next) => {
 router.get("/:idUser", async (req, res, next) => {
   try {
     const oneUser = await User.findById(req.params.idUser);
-    res.render("admin/users", {oneUser});
+    res.render("admin/users", { oneUser });
   } catch (error) {
     next(error);
   }
@@ -29,16 +30,33 @@ router.get("/:idUser/edit", async (req, res, next) => {
   try {
     const editUser = await User.findById(req.params.idUser).populate("class");
     res.render("admin/users-edit.hbs", editUser);
-  } catch (error) {next(error)}
+  } catch (error) {
+    next(error);
+  }
 });
 
 //POST /admin/:idUser/edit
-router.post("/:idUser/edit", async (req, res, next) => {
+router.post("/:idUser/edit",uploader.single("image"), async (req, res, next) => {
   try {
-
-    await User.findByIdAndUpdate(req.params.idUser);
-    res.render("admin/users-edit.hbs");
-  } catch (error) {next(error)}
+    const { email, password, firstName, lastName } = req.body;
+    let profileImg = "";
+    if (req.file === undefined) {
+      profileImg = undefined;
+    } else {
+      profileImg = req.file.path;
+    }
+    console.log(req.body)
+    await User.findByIdAndUpdate(req.params.idUser, {
+      email,
+      password,
+      firstName,
+      lastName,
+      image: profileImg,
+    });
+    res.redirect("/admin");
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
