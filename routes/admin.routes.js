bde39ace406e1b5429cdf7748110a0394f12b8c9
MaishaFN <jose.maisha@gmail.com>
2023-05-16 12:10:42 +0200

    admin routes done

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
 const Class = require("../models/Class.model");
-const uploader = require('../middlewares/cloudinary.middleware.js');
+const uploader = require("../middlewares/cloudinary.middleware.js");
 
 const { isAdmin } = require("../middlewares/middlewares");
 
@@ -11,7 +11,7 @@ router.use(isAdmin);
 router.get("/", async (req, res, next) => {
   try {
     const allUsers = await User.find();
-    res.render("admin/index", {allUsers});
+    res.render("admin/index", { allUsers });
   } catch (error) {
     next(error);
   }
@@ -20,8 +20,12 @@ router.get("/", async (req, res, next) => {
 //GET /admin/:idUser
 router.get("/:idUser", async (req, res, next) => {
   try {
-    const oneUser = await User.findById(req.params.idUser).populate('tutorClass');
-    const userClasses = await User.findById(req.params.idUser).populate('class').select({class: 1});
+    const oneUser = await User.findById(req.params.idUser).populate(
+      "tutorClass"
+    );
+    const userClasses = await User.findById(req.params.idUser)
+      .populate("class")
+      .select({ class: 1 });
     res.render("admin/users", { oneUser, userClasses });
   } catch (error) {
     next(error);
@@ -39,27 +43,104 @@ router.get("/:idUser/edit", async (req, res, next) => {
 });
 
 //POST /admin/:idUser/edit
-router.post("/:idUser/edit",uploader.single("image"), async (req, res, next) => {
-  try {
-    const { email, password, firstName, lastName } = req.body;
-    let profileImg = "";
-    if (req.file === undefined) {
-      profileImg = undefined;
-    } else {
-      profileImg = req.file.path;
+router.post(
+  "/:idUser/edit",
+  uploader.single("image"),
+  async (req, res, next) => {
+    try {
+      const { email, password, firstName, lastName } = req.body;
+      let profileImg = "";
+      if (req.file === undefined) {
+        profileImg = undefined;
+      } else {
+        profileImg = req.file.path;
+      }
+      console.log(req.body);
+      await User.findByIdAndUpdate(req.params.idUser, {
+        email,
+        password,
+        firstName,
+        lastName,
+        image: profileImg,
+      });
+      res.redirect("/admin");
+    } catch (error) {
+      next(error);
     }
-    console.log(req.body)
-    await User.findByIdAndUpdate(req.params.idUser, {
-      email,
-      password,
-      firstName,
-      lastName,
-      image: profileImg,
-    });
-    res.redirect("/admin");
+  }
+);
+
+//POST /admin/:idUser/delete
+router.post("/:idUser/delete", async (req, res, next) => {
+  try {
+    await User.findByIdAndDelete(req.params.idUser);
+    res.redirect("/");
   } catch (error) {
     next(error);
   }
 });
+
+//GET  /admin/class/class
+router.get("/class/class", async (req, res, next) => {
+  try {
+    const allClasses = await Class.findById();
+    res.render("admin/classes-index.hbs", { allClasses });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//GET /admin/class/:idClass
+router.get("/class/:idClass", async (req, res, next) => {
+  try {
+    const oneClass = await Class.findById(req.params.idClass).populate(
+      "alumns"
+    );
+    const professors = await User.find({ class: oneClass._id });
+    res.render("admin/classes.hbs", { oneClass, professors });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//GET /admin/class/:idClass/edit
+router.get("/class/:idClass/edit", async (req, res, next) => {
+  try {
+    const editClass = await Class.findById(req.params.idClass).populate(
+      "alumns"
+    );
+    res.render("admin/classes-edit.hbs", {editClass});
+  } catch (error) {
+    next(error);
+  }
+});
+
+//POST /admin/class/:idClass/edit
+router.post("/class/:idUser/edit", async (req, res, next) => {
+  try {
+    const { name, subname, Subject, alumns } = req.body;
+    await Class.findByIdAndUpdate(req.params.isClass, {
+      name,
+      subname,
+      Subject,
+      alumns,
+    });
+    res.redirect("/admin/class");
+  } catch (error) {
+    next(error);
+  }
+});
+
+//POST /admin/class/:idClass/delete
+router.post("/class/:idClass/delete", async(req, res, next)=>{
+  try {
+    await Class.findByIdAndDelete(req.params.idUser)
+    res.redirect("/admin/class")
+  } catch (error) {
+    next(error)
+  }
+})
+
+
 
 module.exports = router;
