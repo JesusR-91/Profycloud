40a719d2e54c14fc39fbd9069cb11c1ab9334568
Jesús Ruiz <jesusruizjaen@gmail.com>
2023-05-17 18:01:05 +0200

    comment added to the home page

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,6 +3,8 @@ const router = require('express').Router();
 //Models
 const Alumn = require('../models/Alumn.model');
 const Class = require('../models/Class.model');
+const Comment = require('../models/Comment.model');
+const User = require('../models/User.model');
 
 //Middlewares
 const {isLoggedIn, updateLocals, adminLocals} = require('../middlewares/middlewares');
@@ -15,16 +17,28 @@ router.use('/', authRouter);
 
 router.use(isLoggedIn);
 
-router.get("/", (req, res, next) => {
-  res.render("index");
+router.get("/", async (req, res, next) => {
+  try {
+    const user = await User.findById(req.session.user._id).populate('tutorClass');
+    const alumns = await Alumn.find({classroom: `${user.tutorClass.name} ${user.tutorClass.subName}`});
+    const allComments = await Comment.find({madeTo: alumns}).populate('madeBy');
+    const alumnsComments = await Comment.find({madeTo: alumns}).populate('madeTo').select({madeTo: 1})
+
+    alumnsComments.forEach(alumn =>{
+      allComments.forEach(comment => {
+        comment.madeTo = alumn.madeTo;
+      })
+    })
+    res.render("index", {allComments});
+  } catch (error) {next(error)}
 });
 
-//POST for the finder
+//POST '/' => for the finder
 
-router.post('/find', async (req, res, next)=>{
+router.post('/', async (req, res, next)=>{
   try {
   const {find} = req.body;
-  let regexLet = /^(?=.*[a-zA-Z])$/gm;
+  // let regexLet = /^(?=.*[a-zA-Z])$/gm;
 
   if ((find !== undefined) && (find.length <= 2 && find.length > 0)) {
     const foundClass = await Class.findOne({$and: [{name: find[0]}, {subName: find[1]}]});
@@ -34,7 +48,6 @@ router.post('/find', async (req, res, next)=>{
     res.redirect(`/class/${foundClass._id}`)
   } else if ((find !== undefined) && (find.length > 3)) {
     let alumn = find.split(' ');
-    console.log(alumn)
     if(alumn.length === 1) {
       res.redirect(`/alumn/find-list/${alumn[0]}`);
     } else {
@@ -45,6 +58,7 @@ router.post('/find', async (req, res, next)=>{
     res.render('index.hbs', {
       errorMessage: 'Try to be more specific.'
     })
+    return;
   }
 
   } catch (error) { next(error)}
