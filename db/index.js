fbf8fddd6b119eef4ebc12a495cf132e1f55a6c9
Jesús Ruiz <jesusruizjaen@gmail.com>
2023-05-18 13:05:46 +0200

    css

--- db/index.js
+++ db/index.js
@@ -5,8 +5,8 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-// const MONGO_URI = "mongodb+srv://JesusR:yQZP6O8fP9GXzbsP@cluster0.ptiocfs.mongodb.net/profycloud?retryWrites=true&w=majority";
-const MONGO_URI = "mongodb://127.0.0.1:27017/proyect2";
+const MONGO_URI = "mongodb+srv://JesusR:yQZP6O8fP9GXzbsP@cluster0.ptiocfs.mongodb.net/profycloud?retryWrites=true&w=majority";
+// const MONGO_URI = "mongodb://127.0.0.1:27017/proyect2";
 
 mongoose
   .connect(MONGO_URI)
